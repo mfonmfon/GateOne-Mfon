6a584eddcd8d40be469648e04b7521c89aaf2fa0
mfonmfon <mfonm579@gmail.com>
2024-05-14 17:27:13 +0100

    commit

--- DaySeven/DayOne/java/ATM machine/AtmMachine.java
+++ DaySeven/DayOne/java/ATM machine/AtmMachine.java
@@ -5,7 +5,9 @@ public class AtmMachine{
 	public static void main(String... args){
 	Scanner scan = new Scanner(System.in);
 
-	String userPin = "8";
+
+
+	String userPin = "4";
 	System.out.println("""
 
 ____________________________________________
@@ -64,11 +66,11 @@ ____________________________________________
 				System.out.println("Enter your pin: ");
 				userPin = scan.next();
 
-					if(userPin.length() < 8){
+					if(userPin.length() < 4){
 						System.out.println("you ATM pin can not be lower than 8");
 						break;
 					}
-					if(userPin.length() > 8){
+					if(userPin.length() > 4){
 						System.out.println("Your ATM pin can only have 8 character ");
 						break;
 
@@ -80,11 +82,102 @@ ____________________________________________
 
 					}
 
-				}
-
 				
 
+			case 4:
+				System.out.println("Select your language preference");
 
+				System.out.println("""
+				_____________________________________________________
+				..................................................... 
+						
+						Select Language	
+				_____________________________________________________
+				.....................................................
+					1. Yoruba
+					2. English
+					3. Igbo
+					4. Ibibio
+					5. Hausa
+				
+				""");
+
+				int userLanguage = scan.nextInt();
+			
+				switch(userLanguage){
+
+					case 1:
+						System.out.println("Sorry! The Yoruba language is not up to date on this machine. ");
+						break;
+					case 2:
+						System.out.println("Enter your 4 digit ATM pin ");
+						userPin = scan.next();
+
+							if(userPin.length() > 4){
+								System.out.println("Incorrec Pin.\nYour pin must be 4 digit only");								
+							}
+							else if(userPin.length() < 4){
+								System.out.println("Incorrec Pin.\nYour pin must be 4 digit only");
+								
+							}else{
+								System.out.println("""
+									
+							____________________________________________________________
+ 							............................................................
+									
+											Account Type
+								____________________________________________________
+								....................................................
+										1. Savings Account
+		
+										2. Current Account
+
+								""");
+							       int accountType = scan.nextInt();
+
+								switch(accountType){
+
+									case 1:
+										System.out.println("Enter the amount you want to withdraw");
+										
+										System.out.println(""" 
+										1. 1000       4. 10000
+										
+										2. 2000	      5. 20000
+
+										3. 5000
+
+										
+
+
+
+										""");
+
+										int withdrawAmount = scan.nextInt();
+
+										
+								};
+
+
+							}
+						System.out.println("");
+						break;
+
+					case 3:
+						System.out.println("Sorry! The Igbo language is not up to date on this machine. ");
+						break;
+					case 4: 
+						System.out.println("Sorry! The Ibibio language is not up to date on this machine. ");
+						break;
+					case 5:
+						System.out.println("Sorry! The Hausa language is not up to date on this machine. ");
+						break;
+
+				
+				}
+				
+
+			}
 		}
 	
 	
