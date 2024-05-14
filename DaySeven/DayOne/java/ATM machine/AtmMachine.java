d1dfd46688846303ac3017952df37cb254b40d7d
mfonmfon <mfonm579@gmail.com>
2024-05-14 10:46:29 +0100

    commit

--- DaySeven/DayOne/java/ATM machine/AtmMachine.java
+++ DaySeven/DayOne/java/ATM machine/AtmMachine.java
@@ -1,12 +1,21 @@
 import java.util.Scanner;
+
 public class AtmMachine{
 
 	public static void main(String... args){
 	Scanner scan = new Scanner(System.in);
 
-	String options = """ 
-	
-	1. Create an account with their first name, last name and pin
+	String userPin = "8";
+	System.out.println("""
+
+____________________________________________
+.............................................
+
+	    BANKE BANK 
+____________________________________________
+.............................................
+
+	1. Create an account 
 
 	2. Close account
 
@@ -20,34 +29,65 @@ public class AtmMachine{
 
 	7. Change Pin
 
-	""";
+	""");
+	int option = scan.nextInt();
+			
+	
+		
+		
 
+		switch(option){
+			case 1:
+				System.out.println("Open an account to enjoy safe, versatile and convenient banking with Banke Bank");
+				System.out.println();
+		
+				System.out.println("Let's get started with your BVN or NIN\nDial #565*0# to check your Bank Verification Number (BVN)");
+				System.out.println();
 
-		switch(options){
+				System.out.print("Enter your BVN ");
+				Long bankVeriNumber = scan.nextLong();
 
-			case1:
-				System.out.println("Create an account");
+				System.out.print("Enter your Date Of Birth");
+				String dateOfBirth = scan.next();
+
+				System.out.print("Enter your NIN");
+				Long idNumber = scan.nextLong();
+
+				System.out.println("Let's get started with your Name and pin");
 
 				System.out.println("Enter your first name: ");
-				String firstName = scan.next();
+				String userFirstName = scan.next();
 
 				System.out.println("Enter your second name: ");
-				String secondName = scan.next();
+				String userSecondName = scan.next();
 
-				System.out.println("Enter your pin");
-				int userPin = scan.nextInt();
+				System.out.println("Enter your pin: ");
+				userPin = scan.next();
 
-				if(userPin.equals(userPin)){
-					System.out.println("valid"); 
+					if(userPin.length() < 8){
+						System.out.println("you ATM pin can not be lower than 8");
+						break;
+					}
+					if(userPin.length() > 8){
+						System.out.println("Your ATM pin can only have 8 character ");
+						break;
+
+					}
+					else{
+						System.out.println("Hello " + userFirstName + " " + userSecondName + " "  );
+						System.out.println("your account  was created successfully>>>>>>>>>");
+
+
+					}
 
 				}
-				System.out.println(options);
 
-
+				
 
 
 		}
-	}
+	
+	
 
 
 }
