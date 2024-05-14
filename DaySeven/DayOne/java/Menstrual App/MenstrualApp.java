104943d0272f11fbac75048565497cae5ea590e5
mfonmfon <mfonm579@gmail.com>
2024-05-14 17:23:56 +0100

    commit

--- DaySeven/DayOne/java/Menstrual App/MenstrualApp.java
+++ DaySeven/DayOne/java/Menstrual App/MenstrualApp.java
@@ -5,13 +5,14 @@ public class MenstrualApp{
 
 	public static void main(String... args){
 	Scanner scan = new Scanner(System.in);
-	System.out.println("Welcome to MyChekker helathcare, ");
+	System.out.println("Welcome to MyChekker healthcare, ");
 	System.out.println();
 	System.out.println("We are on a mission to improve the well-being of all women");
 	System.out.println(""" 
 	Your data is very secure with us here
 	
 	1. -> signUp
+
 	2. -> Login
 	
 	""");
@@ -28,7 +29,14 @@ public class MenstrualApp{
 			System.out.println("Opoos!! The age you entered is below the normal age we expect for adults check the programme again  or\n This menstrual app can only be checked for those only from the age 12 ");
 
 		}
-		//else{
+
+
+
+
+				System.out.println("Hello " + userName + "," + "welcome to MyChekker healthcare where you can keep track of your period.\nTrust us all you informations are safe and private with us. ");
+
+
+		
 	case 2: 
 	System.out.println("Answer the following questions to calculate when next your period will come");
 	System.out.println();
@@ -54,15 +62,20 @@ __________________________________________________________
 	7. Dry discharge
 ___________________________________________________________
 ...........................................................
+	
 	 """);
-	int periodSigns = scan.nextInt();
 
-
-System.out.println("Select the options above ");
+	
+	System.out.println("Select the options above ");
 	int questions = scan.nextInt();
+
+	
+	
 	switch(questions){
 
 	case 1:
+		System.out.println("This is a one of the symthoms that tells that your period is aproaching!");
+
 		System.out.print("When did your last period start: ");
 		String beginningOfPeriod = scan.next();
 
@@ -75,8 +88,9 @@ System.out.println("Select the options above ");
 		//LocalDate date = LocalDate.now();
 		break;
 		
-
 	case 2:
+		System.out.println("This is a one of the symthoms that tells that your period is aproaching!");
+
 		System.out.print("When did your last period start: ");
 		beginningOfPeriod = scan.next();
 
@@ -87,8 +101,9 @@ System.out.println("Select the options above ");
 		cycleLength = scan.nextInt();
 		break;
 
-
 	case 3:
+		System.out.println("This is a one of the symthoms that tells that your period is aproaching!");
+
 		System.out.print("When did your last period start: ");
 		beginningOfPeriod = scan.next();
 
@@ -98,9 +113,9 @@ System.out.println("Select the options above ");
 		System.out.print("What is the average length of the menstruation: ");
 		cycleLength = scan.nextInt();
 
-
-
 	case 4:
+		System.out.println("This is a one of the symthoms that tells that your period is aproaching!");
+
 		System.out.print("When did your last period start: ");
 		beginningOfPeriod = scan.next();
 
@@ -113,6 +128,8 @@ System.out.println("Select the options above ");
 
 
 	case 5:
+		System.out.println("This is a one of the symthoms that tells that your period is aproaching!");
+
 		System.out.print("When did your last period start: ");
 		beginningOfPeriod = scan.next();
 
@@ -123,8 +140,9 @@ System.out.println("Select the options above ");
 		cycleLength = scan.nextInt();
 		break;
 
-
 	case 6:
+		System.out.println("This is a one of the symthoms that tells that your period is aproaching!");
+
 		System.out.print("When did your last period start: ");
 		beginningOfPeriod = scan.next();
 
@@ -133,10 +151,11 @@ System.out.println("Select the options above ");
 
 		System.out.print("What is the average length of the menstruation: ");
 		cycleLength = scan.nextInt();
-
 		break;
 
 	case 7:
+		System.out.println("This is a one of the symthoms that tells that your period is aproaching!");
+
 		System.out.print("When did your last period start: ");
 		beginningOfPeriod = scan.next();
 
@@ -146,10 +165,12 @@ System.out.println("Select the options above ");
 		System.out.print("What is the average length of the menstruation: ");
 		cycleLength = scan.nextInt();
 
-
 	default:
-
 		System.out.println("Oops!! you entered the wrong input...");
+
+
+
+	
 
 	
 	
