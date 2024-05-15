29ba2d392705a8821e50a717363dc094a0f4854c
mfonmfon <mfonm579@gmail.com>
2024-05-15 15:25:14 +0100

    commit

---
+++ DaySeven/DayOne/javaScript/menstrualApp.js
@@ -0,0 +1,12 @@
+console.log("Welcome to MyChekker healthCare");
+console.log();
+console.log("We are on a mission to improve the well-being of all women");
+
+console.log(""" 
+
+	1. -> SignUp
+
+	2. -> Login
+
+	""");
+
