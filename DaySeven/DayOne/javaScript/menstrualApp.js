661e63795439e91079fccb292911abe328e83d72
mfonmfon <mfonm579@gmail.com>
2024-05-15 17:25:20 +0100

    commit

--- DaySeven/DayOne/javaScript/menstrualApp.js
+++ DaySeven/DayOne/javaScript/menstrualApp.js
@@ -1,12 +1,43 @@
-console.log("Welcome to MyChekker healthCare");
-console.log();
-console.log("We are on a mission to improve the well-being of all women");
+const prompt = require("prompt-sync")();
 
-console.log(""" 
+
+	console.log("Welcome to MyChekker healthcare");
+	console.log();
+	console.log("We are on a mission to improve the well-being of all women");
+ let options = ("""	
+	Your Data is very secure with us here, enter 1 to signUp or 2 to Login
 
 	1. -> SignUp
 
 	2. -> Login
 
-	""");
+""");
+console.log("Based on the date of your last menstrual period and the length of your typical menstrual cycle,\n discover which days you're most likely to be fertile and boost your chance of conceiving.");
+	
+let userName = prompt("Enter the first day you saw your period ");
+cycleLength = prompt("What is your average cycle length");
+
+
+
+
+
+
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
